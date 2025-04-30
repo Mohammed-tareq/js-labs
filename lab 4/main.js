1471de373b901eed7493b748f714288a36395e33
Mohammed tareq <Mohtareq1999m@gmail.com>
2025-05-01 02:36:43 +0300

    lab4

--- lab 4/main.js
+++ lab 4/main.js
@@ -13,6 +13,39 @@
 
 // =====================================================================================================================
 
-let city = document.getElementById("city");
-let optin =city.options[city.selectedIndex].text;
-console.log(optin);
+// let city = document.getElementById("city");
+//
+// console.log(city.innerText);
+//
+
+//======================================================================================================================
+//
+// let table = document.querySelectorAll("table");
+// console.log(table.item(1).innerText);
+
+// //======================================================================================================================
+//  let className = doucment.querySelectorAll(".fontBlue .BGrey");
+// let className = doucment.getElementsByClassName("fontBlue");
+// let className = doucment.getElementsByClassName("BGrey");
+
+//======================================================================================================================
+// function updateTimeTitle(){
+//     let time = new Date();
+//     document.title = "time is " + time;
+// }
+// setInterval(updateTimeTitle,10000);
+
+//======================================================================================================================
+ let seachUrl = location.search;
+let handelUrl = seachUrl.substring(1);
+let urlToArr = handelUrl.split('&');
+let obj ={};
+urlToArr.forEach((elemant)=>{
+    let url =elemant.split('=');
+    obj[url[0]] = url[1];
+})
+
+console.log(obj);
+
+
+
